c1d1205ff63569e3d0c70e58b0ff1b85e8456f6c
Michael Lynch <ml849@hotmail.com>
2025-02-17 19:25:40 -0500

    working header and skeleton fixed

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+
+
+//HeaderMainButton text="pls work"/>
+
+    return <header>
+        Epic header that totally works
+    </header>
+
+} export default Header
