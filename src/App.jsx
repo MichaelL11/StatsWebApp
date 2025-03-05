52cb97e6b16b535d940c234528a746df48a74f23
Michael Lynch <ml849@hotmail.com>
2025-03-05 00:28:35 -0500

    I dont even care anymore

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/leader" element={<LeaderboardPage />} />
         </Routes>
-        other stuff
     </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Box, Button } from '@mui/material';
+import myImage from './assets/PbaLogo2.png';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the PBA! Go </h1>
-            <p>Welcome to the homepage!</p>
-        </div>
+        <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            height="100%" // Header height
+            width="100%"
+            padding="0 20px"
+        >
+            <Box
+                sx={{
+                    display: 'block',
+                    width: '100%', // Set the width of the logo box
+                    height: '100%', // Ensure it fills the full height of the header
+                    backgroundImage: `url(${myImage})`, // Background image
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat', // Prevent repetition of the image
+                }}
+            />
+            {/* Content of the homepage */}
+            <Box>
+                Poki Sucks
+            </Box>
+        </Box>
     );
 }
 
